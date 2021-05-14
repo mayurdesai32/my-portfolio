b09009ea4cdbb36f34fbd2ddfc7e8cacccca3d77
Mayur Sadashiv Desai <76611053+mayurdesai32@users.noreply.github.com>
2021-05-15 00:53:48 +0530

    logo changed

--- client/src/component/Footer.js
+++ client/src/component/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../logo/apple-touch-icon.png';
+import logo from '../logo/new/logod.png';
 const Footer = () => {
   return (
     <div className='footer'>

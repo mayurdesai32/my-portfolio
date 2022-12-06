071811e982b672877d477cfccb04c9e5c9992d28
Mayur Sadashiv Desai <76611053+mayurdesai32@users.noreply.github.com>
2022-12-06 21:19:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
+// const dotenv = require('dotenv');
+// dotenv.config({ path: './config.env' });
 const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
